8dea3386a364ea078e32114f42538813f30ba7ae
OlgaBogush <bohush.olga@gmail.com>
2025-07-25 12:46:30 +0300

    fix: correct burger menu

--- js/burger.js
+++ js/burger.js
@@ -1,17 +1,36 @@
 export default function toggleBurger() {
-
   const burgerIcon = document.querySelector(".burger-icon")
   const burgerMenu = document.querySelector(".burger-menu")
+  const burgerOverlay = document.querySelector(".burger-overlay")
   const welcomeTitle = document.querySelector(".welcome-title")
 
   burgerIcon.addEventListener("click", () => {
-    burgerIcon.classList.toggle("active")
-    if(burgerIcon.classList.contains("active")) {
-      welcomeTitle.classList.add("hidden")
-      burgerMenu.classList.add("open")
+    if (burgerIcon.classList.contains("active")) {
+      hideMenu()
     } else {
-      welcomeTitle.classList.remove("hidden")
-      burgerMenu.classList.remove("open")
+      openMenu()
     }
+    burgerMenu.addEventListener("click", () => {
+      hideMenu()
+    })
+    burgerOverlay.addEventListener("click", () => {
+      hideMenu()
+    })
   })
+
+  function openMenu() {
+    burgerIcon.classList.add("active")
+    burgerMenu.classList.add("open")
+    burgerOverlay.classList.add("active")
+    welcomeTitle.classList.add("hidden")
+    document.documentElement.style.overflow = "hidden"
+  }
+  function hideMenu() {
+    burgerIcon.classList.remove("active")
+    burgerMenu.classList.remove("open")
+    burgerOverlay.classList.remove("active")
+    welcomeTitle.classList.remove("hidden")
+    document.documentElement.style.overflow = ""
+  }
+
 }
